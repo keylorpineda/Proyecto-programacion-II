657fd1d8a2ea29200fbedc7fdbb30e9f39936854
keylorpineda <164683707+keylorpineda@users.noreply.github.com>
2025-03-24 12:06:52 -0600

    commit #2: Se hizo el registro de un usuario y se añadieron alertas.

--- src/main/java/Controllers/SignUpWindowController.java
+++ src/main/java/Controllers/SignUpWindowController.java
@@ -4,9 +4,18 @@
  */
 package Controllers;
 
+import com.mycompany.proyectoprogramacionii.User;
+import com.mycompany.proyectoprogramacionii.Utilities;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 /**
  * FXML Controller class
@@ -14,13 +23,66 @@ import javafx.fxml.Initializable;
  * @author Keylo
  */
 public class SignUpWindowController implements Initializable {
+    @FXML
+    private TextField txtName;
+    @FXML
+    private TextField txtLastName;
+    @FXML
+    private TextField txtId;
+    @FXML
+    private TextField txtUserName;
+    @FXML
+    private TextField txtPassword;
+    @FXML
+    private RadioButton rdbCustomer;
+    @FXML
+    private RadioButton rdbAdministrator;
+    @FXML
+    private Button btnCreateAccount;
+    @FXML
+    private ToggleGroup roles;
+
+    private Utilities utilities;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        utilities = new Utilities();
+        roles = new ToggleGroup();
+    }
+
+    @FXML
+    private void clickOnCreate(ActionEvent event) {
+        rdbCustomer.setToggleGroup(roles);
+        rdbAdministrator.setToggleGroup(roles);
+        String name = txtName.getText().trim(); 
+        String lastName = txtLastName.getText().trim();
+        String id = txtId.getText().trim();
+        String userName = txtUserName.getText().trim();
+        String password = txtPassword.getText().trim();
+        String role = "";
+
+        if (name.isEmpty() || lastName.isEmpty() || id.isEmpty() || userName.isEmpty() || password.isEmpty()) {
+            utilities.showAlert(AlertType.ERROR, "Campos incompletos", "Por favor, complete todos los campos.");
+         return;
+        }
+        if (rdbCustomer.isSelected()) {
+            role = "CUSTOMER";
+        } else if (rdbAdministrator.isSelected()) {
+            role = "ADMINISTRATOR";
+        } else {
+            utilities.showAlert(AlertType.ERROR, "Rol no seleccionado", "Por favor, seleccione un rol.");
+            return;
+        }
+
+        if (password.length() < 6) {
+            utilities.showAlert(AlertType.ERROR, "Contraseña debil", "La contraseña debe tener al menos 6 caracteres.");
+            return;
+        }
+        User newUser = new User(name, lastName, id, userName, password, role);
+        utilities.showAlert(AlertType.INFORMATION, "Usuario creado", "Usuario creado exitosamente.");
+    }
+
 }
